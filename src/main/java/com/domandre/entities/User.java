69edb154bdb6628bd4e2f22dfd8deec530c02270
Domingos André <domandreqs@gmail.com>
2025-02-13 00:19:23 +0000

    First steps to implement jwt

---
+++ src/main/java/com/domandre/entities/User.java
@@ -0,0 +1,28 @@
+package com.domandre.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
